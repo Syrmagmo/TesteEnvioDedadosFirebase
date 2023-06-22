54f3a5522aae0500e73fec04da47ef18fb65d483
Syr Magmo <lincolnrichard2@gmail.com>
2023-06-22 00:15:03 -0300

    arrumando aos poucos

--- app/src/main/java/com/example/testeenviodedadosfirebase/Fragment/Fragment_Add_livro.java
+++ app/src/main/java/com/example/testeenviodedadosfirebase/Fragment/Fragment_Add_livro.java
@@ -1,5 +1,256 @@
 package com.example.testeenviodedadosfirebase.Fragment;
 
+
+
+//\/\/\/\/\/\ codigo a baixo esta quase funcionando
+/*
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.testeenviodedadosfirebase.R;
+import com.example.testeenviodedadosfirebase.model.livroModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.card.MaterialCardView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Fragment_Add_livro extends Fragment {
+    //Dados do livro \/
+    private EditText edTituloDoLivro, edAutorDoLivro, edAnoDoLivro, edDescricaoDoLivro;
+    private Spinner spGeneroDoLivro, spEstadoDoLivro;
+    //Dados do livro /\
+
+    private FirebaseAuth firebaseAuth;
+    private String UsuarioAtualID;
+
+    private String LivroId;
+    private MaterialCardView Escolherfoto; // Foi usado para executar uma ação ao clicar no card
+    private Uri ImageUri; // Foi usado para armazenar o dado da imagem
+    private Bitmap bitmap; // Foi usado para converter a imagem para bitmap
+    private ImageView FotoLivroImageView;
+    private String FotoUrl;
+    private FirebaseStorage Storage; // Método para salvar a imagem para o Storage do Firebase
+
+    private FirebaseDatabase BancoTempoReal; // Método para chamar o banco de dados
+
+    private StorageReference mStorageRef; // Método para referenciar os dados para o Storage
+
+    // Progress dialog para exibir o progresso do envio do livro
+    private ProgressDialog progressDialog;
+
+    public Fragment_Add_livro() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_add_livro, container, false);
+
+        edTituloDoLivro = rootView.findViewById(R.id.EditText_Titulo_Livro);
+        edAutorDoLivro = rootView.findViewById(R.id.EditText_Autor_Livro);
+        spGeneroDoLivro = rootView.findViewById(R.id.EditSP_Genero_Livro);
+        edAnoDoLivro = rootView.findViewById(R.id.EditText_Ano_Livro);
+        spEstadoDoLivro = rootView.findViewById(R.id.Spinner_Estado_Livro);
+        edDescricaoDoLivro = rootView.findViewById(R.id.editTextTextMultiLine_Descricao);
+
+        // instanciando o banco
+        BancoTempoReal = FirebaseDatabase.getInstance();
+        Storage = FirebaseStorage.getInstance();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        // para obter o id do usuário atual
+        UsuarioAtualID = firebaseAuth.getCurrentUser().getUid();
+
+        // Torna o primeiro parâmetro do Spinner Nulo
+        Spinner spinner = rootView.findViewById(        R.id.EditSP_Genero_Livro);
+
+        // Configurando o Adapter para o Spinner de Gênero do Livro
+        ArrayAdapter<CharSequence> generoAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.generos_livros, android.R.layout.simple_spinner_item);
+        generoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spGeneroDoLivro.setAdapter(generoAdapter);
+
+        Escolherfoto = rootView.findViewById(R.id.escolher_foto);
+
+        FotoLivroImageView = rootView.findViewById(R.id.Inserir_foto_imageView);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        Escolherfoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPermission();
+            }
+        });
+
+        return rootView;
+    }
+
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            } else {
+                pickImage();
+            }
+        } else {
+            pickImage();
+        }
+    }
+
+    private void pickImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                pickImage();
+            } else {
+                Toast.makeText(getActivity(), "Permissão negada", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK && data != null) {
+            ImageUri = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), ImageUri);
+                FotoLivroImageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Método para enviar os dados para o banco de dados
+    private void EnviarDadosDoLivro() {
+        String TituloDoLivro = edTituloDoLivro.getText().toString();
+        String AutorDoLivro = edAutorDoLivro.getText().toString();
+        String GeneroDoLivro = spGeneroDoLivro.getSelectedItem().toString();
+        String AnoDoLivro = edAnoDoLivro.getText().toString();
+        String EstadoDoLivro = spEstadoDoLivro.getSelectedItem().toString();
+        String DescricaoDoLivro = edDescricaoDoLivro.getText().toString();
+
+        if (TextUtils.isEmpty(TituloDoLivro)) {
+            Toast.makeText(getActivity(), "Por favor, insira o título do livro", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(AutorDoLivro)) {
+            Toast.makeText(getActivity(), "Por favor, insira o nome do autor do livro", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(AnoDoLivro)) {
+            Toast.makeText(getActivity(), "Por favor, insira o ano do livro", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(DescricaoDoLivro)) {
+            Toast.makeText(getActivity(), "Por favor, insira uma descrição do livro", Toast.LENGTH_SHORT).show();
+        } else {
+            progressDialog.setTitle("Enviando livro");
+            progressDialog.setMessage("Aguarde enquanto enviamos o livro...");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            ArmazenarImagemNoFirebase();
+        }
+    }
+
+    // Método para armazenar a imagem no Firebase Storage
+    private void ArmazenarImagemNoFirebase() {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+        byte[] imageData = byteArrayOutputStream.toByteArray();
+
+        StorageReference filepath = mStorageRef.child("Imagens").child(edTituloDoLivro + ".jpg");
+        UploadTask uploadTask = filepath.putBytes(imageData);
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        String downloadUrl = uri.toString();
+                        SalvarDadosNoBancoDeDados(downloadUrl);
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(getActivity(), "Erro ao enviar a imagem", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    // Método para salvar os dados do livro no banco de dados
+    private void SalvarDadosNoBancoDeDados(String imagemUrl) {
+        DatabaseReference livroRef = FirebaseDatabase.getInstance().getReference().child("Livros").push();
+
+        Map<String, Object> livroMap = new HashMap<>();
+        livroMap.put("titulo", TituloDoLivro);
+        livroMap.put("autor", AutorDoLivro);
+        livroMap.put("genero", GeneroDoLivro);
+        livroMap.put("ano", AnoDoLivro);
+        livroMap.put("estado", EstadoDoLivro);
+        livroMap.put("descricao", DescricaoDoLivro);
+        livroMap.put("imagem", imagemUrl);
+
+        livroRef.setValue(livroMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    progressDialog.dismiss();
+                    Toast.makeText(getActivity(), "Livro enviado com sucesso", Toast.LENGTH_SHORT).show();
+                } else {
+                    progressDialog.dismiss();
+                    Toast.makeText(getActivity(), "Erro ao enviar o livro", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
+*/
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CODIGO A CIMA ESTA QUASE PRONTO
+
+/*
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
@@ -258,115 +509,4 @@ public class Fragment_Add_livro extends Fragment {
         }
     }
 }
-
-/*
-    private void UploadImage(){
-
-        // verifica a imagemUri
-        if (ImageUri != null){
-            final StorageReference myRef=mStorageRef.child("foto/" + ImageUri.getLastPathSegment());
-            myRef.putFile(ImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    // aqui precisa do dowloadUrl do store em string
-                    myRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                        @Override
-                        public void onSuccess(Uri uri) {
-                            if (uri != null){
-                                FotoUrl = uri.toString();
-                            }
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-
-                        }
-                    });
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-
-    }
-
-    // Aqui sera upado as outras informações do livro!
-    private void UploadDadosLivro(){
-
-        String titulo = edTituloDoLivro.getText().toString().trim();
-        String autor = edAutorDoLivro.getText().toString().trim();
-        String genero = spGeneroDoLivro.getSelectedItem().toString();
-        String estado = spEstadoDoLivro.getSelectedItem().toString();
-        String ano = edAnoDoLivro.getText().toString().trim();
-        String descricao = edDescricaoDoLivro.getText().toString().trim();
-
-        //
-
-        if (TextUtils.isEmpty(titulo) &&
-                TextUtils.isEmpty(autor) &&
-                TextUtils.isEmpty(genero) &&
-                TextUtils.isEmpty(estado) &&
-                TextUtils.isEmpty(ano) &&
-                TextUtils.isEmpty(descricao)){
-            Toast.makeText(getContext(), "Por favor preencha todos os campos", Toast.LENGTH_SHORT).show();
-        }else {
-
-            /*
-            Caso fosse utilizado o Firestore, porem estou utilizando o firebase Realtime
-            DocumentReference myRef = BancoTempoReal.collection("LivroInformacoes").document();
-             */
-  /*          DatabaseReference databaseReference = BancoTempoReal.getReference("PostLivro").push();
-
-            livroModel livroModel = new livroModel(titulo,
-                    autor, ano, genero, estado,
-                    descricao,
-                    "","",FotoUrl,"", UsuarioAtualID);
-
-            databaseReference.set(livroModel. SetOptions.merge()).addOnCompleteListener(new OnCompleteListener<void>()
-            {
-                @Override
-                public void onComplete(@NonNull Task<Void> task){
-
-                    if (task.isSuccessful()){
-                        if (task.isSuccessful()){
-
-                            // we need to get doc id and set to model to store in firestore
-                            docID = documentReference.getid();
-                            livroModel.setDocId(docId);
-                            // now this doc id will sent into firestore
-                            documentReference.set(livroModel, SetOptions.merge()).addOnCompleteListener(new OnCompleteListener<void>(){
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if(task.isSuccessful()){
-                                        Toast.makeText(getContext(), "Envio com sucesso!", Toast.LENGTH_SHORT).show();
-
-                                }
-                                }).addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                            }
-
-                        }
-                    }
-
-                }
-            }).addOnFailureListener(new OnFailureListener(){
-                @Override
-                public void OnFailureListener(@NonNull Exception e){
-                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-
-                }
-            });
-/*
-        }
-
-
-    }
-
-}*/
+*/
